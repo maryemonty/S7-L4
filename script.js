12cd3cc5f97165738a5bc8183a7a9e8d0f5142ff
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-03-16 15:37:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const key = 'MQHxqYQXPheoCCut4bGHNRj0gOnEcVzmp771VgFKxiiGvgO4orjA0ooy'
+const api = 'https://api.pexels.com/v1/'
+const headers = {
+        "Authorization": "MQHxqYQXPheoCCut4bGHNRj0gOnEcVzmp771VgFKxiiGvgO4orjA0ooy"
+    }
+
+fetch(api, {headers})
